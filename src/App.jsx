d276aa6a5fefaa7dfeddcdecf2cb4642aa690f87
Pawan-gautam011 <pawangautam11op@gmail.com>
2024-07-22 23:49:04 +0545

    Banner section done

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,21 @@ import Navbar from "./components/Navbar/Navbar"
 import Category from "./components/Category/Category"
 import Category2 from "./components/Category/Category2"
 import Services from "./components/Services/Services"
+import Banner from "./components/Banner/Banner"
+import headphone from "./assets/hero/headphone.png"
 
+const BannerData  = {
+  discount:"30% OFF",
+  title:"Fine Smile",
+  date:"10 Jan to 28 Jan",
+  image:headphone,
+  title2:"Air Solo Bass",
+  title3:"Winter Sale",
+  title4:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, dignissimos?",
+  bgColor:"#ca0b0b",
+};
 
 function App() {
-
-
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden">
 
@@ -16,6 +26,7 @@ function App() {
     <Category/>
     <Category2/>
     <Services/>
+    <Banner data ={BannerData}/>
 
   </div>
   )
